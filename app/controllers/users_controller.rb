17b81cab5874e6ebdebdd239fb409c6c6aeab335
Chris Matthieu <chris@matthieu.us>
2012-02-22 16:26:47 -0700

    initial commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,105 @@
+class UsersController < ApplicationController
+  # GET /users
+  # GET /users.json
+  def index
+    @users = User.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @users }
+    end
+  end
+  
+  def startcalls
+    phonenumbers = params[:phone_number]
+    response = RestClient.get "http://api.tropo.com/1.0/sessions?action=create&token=0e0a2a5742a75c4eacdbe6bb2ea1bebde01102c0955ff913bdee6b9a3ed5039b34fee05bc42b68f745818848&phones=#{params[:phone_number]}"
+    
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { 
+        render json: @users 
+        if response.code == 200
+          puts "Msg sent HTTP/#{response.code}"
+          true
+        else
+          puts "ERROR | HTTP/#{response.code}"
+          false
+        end
+        }
+    end
+
+
+    
+  end
+
+  # GET /users/1
+  # GET /users/1.json
+  def show
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/new
+  # GET /users/new.json
+  def new
+    @user = User.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @user }
+    end
+  end
+
+  # GET /users/1/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  # POST /users.json
+  def create
+    @user = User.new(params[:user])
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render json: @user, status: :created, location: @user }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /users/1
+  # PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+
+    respond_to do |format|
+      if @user.update_attributes(params[:user])
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/1
+  # DELETE /users/1.json
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    respond_to do |format|
+      format.html { redirect_to users_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Conferencecaller::Application.config.secret_token = 'bf9ec271a6e4997b34e0b45b40c9a141c746feba44df52c0e2f7f1e350aaeb11888eb266b4f0f1ff544f9435925cb369311c8ff0377683bb5a5d34fc9676032c'
